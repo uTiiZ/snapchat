f6a11895a93060d7f3ae85165edde1dccb23f42a
Tanguy KERVRAN <t.kervran@jacq.adds>
2018-04-30 17:01:47 +0200

    Initial commit

--- app/src/main/java/com/utiiz/snapchat/fragment/FriendFragment.java
+++ app/src/main/java/com/utiiz/snapchat/fragment/FriendFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -41,7 +42,9 @@ public class FriendFragment extends Fragment {
     public LinearLayout mLinearLayout;
     @NonNull @BindView(R.id.nested_scroll_view) public NestedScrollView mNestedScrollView;
     @NonNull @BindView(R.id.recycler_view) public RecyclerView mRecyclerView;
-    @NonNull @BindView(R.id.snap_stun) public View mSnapStun;
+    @NonNull @BindView(R.id.snap) public View mSnap;
+    @NonNull @BindView(R.id.hands) public View mHands;
+    @NonNull @BindView(R.id.rainbow) public View mRainbow;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,9 +53,15 @@ public class FriendFragment extends Fragment {
         ButterKnife.bind(this, mLinearLayout);
 
         mNestedScrollView.setPadding(0, mNestedScrollView.getPaddingTop() + Snapchat.getStatusBarHeight(getContext()), 0, 0);
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mSnapStun.getLayoutParams();
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mSnap.getLayoutParams();
         params.setMargins(0, params.topMargin + Snapchat.getStatusBarHeight(getContext()), 0, 0);
-        mSnapStun.setLayoutParams(params);
+        mSnap.setLayoutParams(params);
+        ViewGroup.MarginLayoutParams paramsHands = (ViewGroup.MarginLayoutParams) mHands.getLayoutParams();
+        paramsHands.setMargins(0, paramsHands.topMargin + Snapchat.getStatusBarHeight(getContext()), 0, 0);
+        mHands.setLayoutParams(paramsHands);
+        ViewGroup.MarginLayoutParams paramsRainbow = (ViewGroup.MarginLayoutParams) mRainbow.getLayoutParams();
+        paramsRainbow.setMargins(0, paramsRainbow.topMargin + Snapchat.getStatusBarHeight(getContext()), 0, 0);
+        mRainbow.setLayoutParams(paramsRainbow);
 
         List<String> stringList = new ArrayList<>();
         stringList.add("Hello");
@@ -83,7 +92,7 @@ public class FriendFragment extends Fragment {
 
         VerticalOverScrollBounceEffectDecorator decorator = new VerticalOverScrollBounceEffectDecorator(new NestedScrollViewOverScrollDecorAdapter(mNestedScrollView), 2F, DEFAULT_TOUCH_DRAG_MOVE_RATIO_BCK, DEFAULT_DECELERATE_FACTOR);
 
-        decorator.setOverScrollStateListener(new IOverScrollStateListener() {
+        /*decorator.setOverScrollStateListener(new IOverScrollStateListener() {
             @Override
             public void onOverScrollStateChange(IOverScrollDecor decor, int oldState, int newState) {
                 switch (newState) {
@@ -109,19 +118,90 @@ public class FriendFragment extends Fragment {
                         break;
                 }
             }
-        });
+        });*/
+
+        final Boolean[] canBounceBack = {false};
 
         decorator.setOverScrollUpdateListener(new IOverScrollUpdateListener() {
             @Override
             public void onOverScrollUpdate(IOverScrollDecor decor, int state, float offset) {
-                final View view = decor.getView();
-                if (offset > 0) {
-                    // 'view' is currently being over-scrolled from the top.
-                } else if (offset < 0) {
-                    // 'view' is currently being over-scrolled from the bottom.
-                } else {
-                    // No over-scroll is in-effect.
-                    // This is synonymous with having (state == STATE_IDLE).
+
+                switch (state) {
+                    case STATE_IDLE:
+                        // No over-scroll is in effect.
+                        Log.e("STATE", "STATE_IDLE");
+                        break;
+                    case STATE_DRAG_START_SIDE:
+                        // Dragging started at the left-end.
+                        Log.e("STATE", "STATE_DRAG_START_SIDE");
+
+                        mHands.setAlpha(1);
+                        mRainbow.setAlpha(0);
+
+                        canBounceBack[0] = (offset * 0.5F >= mSnap.getHeight() * 0.33F) ? true : false;
+
+                        if (offset <= mSnap.getHeight()) {
+                            if (mSnap.getBackground().getConstantState() != getResources().getDrawable(R.drawable.ic_ghost, null).getConstantState())
+                                mSnap.setBackground(getResources().getDrawable(R.drawable.ic_ghost, null));
+                            mSnap.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + (offset * 0.5F))
+                                    .setDuration(0)
+                                    .start();
+
+                            mRainbow.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + (offset * 0.5F))
+                                    .setDuration(0)
+                                    .start();
+
+                            mHands.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + Snapchat.DPToPixel(5F, getContext()) + (offset * 0.8F))
+                                    .setDuration(0)
+                                    .start();
+                        } else {
+                            if (mSnap.getBackground().getConstantState() != getResources().getDrawable(R.drawable.ic_ghost_laught, null).getConstantState())
+                                mSnap.setBackground(getResources().getDrawable(R.drawable.ic_ghost_laught, null));
+                            mSnap.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + (offset) - (mSnap.getHeight() * 0.5F))
+                                    .setDuration(0)
+                                    .start();
+
+                            mRainbow.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + (offset) - (mSnap.getHeight() * 0.5F))
+                                    .setDuration(0)
+                                    .start();
+
+                            mHands.animate()
+                                    .y(mNestedScrollView.getPaddingTop() + Snapchat.DPToPixel(25F, getContext()) + (offset) - (mSnap.getHeight() * 0.5F))
+                                    .setDuration(0)
+                                    .start();
+                        }
+
+                        break;
+                    case STATE_DRAG_END_SIDE:
+                        // Dragging started at the right-end.
+                        Log.e("STATE", "STATE_DRAG_END_SIDE");
+                        break;
+                    case STATE_BOUNCE_BACK:
+                        Log.e("STATE", "STATE_BOUNCE_BACK");
+
+                        if (canBounceBack[0]) {
+                            mHands.setAlpha(0);
+                            mRainbow.setAlpha(1);
+
+                            mSnap.animate()
+                                    .y(-(Snapchat.PixelsToDP(mRainbow.getHeight() * 2F + mSnap.getHeight() * 3F, getContext())) - Snapchat.getStatusBarHeight(getContext()))
+                                    .setDuration(300)
+                                    .setInterpolator(new DecelerateInterpolator())
+                                    .start();
+                            mRainbow.animate()
+                                    .y(-(Snapchat.PixelsToDP(mRainbow.getHeight() * 2F + mSnap.getHeight() * 3F, getContext())) - Snapchat.getStatusBarHeight(getContext()))
+                                    .setDuration(300)
+                                    .setInterpolator(new DecelerateInterpolator())
+                                    .start();
+                            canBounceBack[0] = false;
+                        }
+
+                        break;
                 }
             }
         });
