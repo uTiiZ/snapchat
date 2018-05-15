4c0474e8f7fb5c21f390c8c442fcb1c5c27cd990
Tanguy KERVRAN <t.kervran@jacq.adds>
2018-05-15 17:30:09 +0200

    Discover

--- assets/emoji.py
+++ assets/emoji.py
@@ -2,7 +2,10 @@ import re
 
 if __name__ == '__main__':
 	f = open('emoji.html', 'rb')
+	txt = open('emoji.txt', 'r+')
+	txt.truncate()
 	for line in f.readlines() :
-		match =  re.match(r".*https://emojipedia-us.s3.amazonaws.com/thumbs/72/facebook/138/(?P<emoji>.*).png", str(line))
+		match =  re.match(r".*https://emojipedia-us.s3.amazonaws.com/thumbs/72/facebook/138/(?P<emoji>.*).png\"", str(line))
 		if match :
+			txt.write(match.group('emoji') + "\n")
 			print(match.group('emoji'))
